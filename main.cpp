5161cbda838f07b8f760f226a015f5014b99a961
Sergey <Dezzer90@mail.ru>
2022-04-04 20:22:54 +0300

    code: масштабирование гистограммы

--- main.cpp
+++ main.cpp
@@ -108,10 +108,25 @@ show_histogram_svg(const vector<size_t>& bins) {
     const auto TEXT_WIDTH = 50;
     const auto BIN_HEIGHT = 30;
     const auto BLOCK_WIDTH = 10;
+    const auto MAX_ASTERISK = IMAGE_WIDTH - TEXT_WIDTH;
+
+    size_t max_count = 0;
+    for (size_t bin : bins) {
+        if (bin * BLOCK_WIDTH > max_count) {
+            max_count = bin * BLOCK_WIDTH;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
     double top = 0;
     for (size_t bin : bins) {
-        const double bin_width = BLOCK_WIDTH * bin;
+        size_t bin_scale = bin;
+        if (scaling_needed) {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            bin_scale = (size_t)(bin * scaling_factor);
+        }
+        const double bin_width = BLOCK_WIDTH * bin_scale;
         svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
         svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
         top += BIN_HEIGHT;
