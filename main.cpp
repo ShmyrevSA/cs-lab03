b01b821b413c6f740b69c86e7434d4ede0b1e031
Sergey <Dezzer90@mail.ru>
2022-05-15 22:19:06 +0300

    code: сообщение об ошибке

--- main.cpp
+++ main.cpp
@@ -89,6 +89,10 @@ main(int argc, char* argv[]) {
             CURLcode res;
             curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
             res = curl_easy_perform(curl);
+            if(res){
+                cerr<<" Error text  = "<<curl_easy_strerror(res);
+                exit(1);
+            }
             curl_easy_cleanup(curl);
         }
         return 0;
